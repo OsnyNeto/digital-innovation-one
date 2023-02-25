77091a4cd25a4517245b7de240424c4b6e985210
OsnyNeto <osnynt@gmail.com>
2023-02-25 03:38:18 -0300

    Inserindo mensagens e recuperando os dados

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        //Importando a classe Scanner
+        Scanner sc = new Scanner(System.in);
+        
+        //Declarando as váriaveis
+        int numeroConta=0;
+        String agencia=null;
+        String nomeCliente=null;
+        Float saldo;
+
+        //Exibindo menssagens para o usuário e recuperando os dados
+        System.out.println("Digite o seu nome! ");
+        nomeCliente = sc.nextLine();
+       
+        System.out.println("Digite o número da conta!");
+        numeroConta = sc.nextInt();        
+       
+        System.out.println("Digite o número da agência!");
+        agencia = sc.next();
+        
+        System.out.println("Digite o valor de depósito!");
+        saldo = sc.nextFloat();
+
+        //Modo de concatenação normal usando o símbolo de mais (+).
+        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+ numeroConta+" e seu saldo "+ saldo+" já está disponível para saque.");
+
+        //Modo de concatenação usando o método concat.
+       System.out.println("Olá " + nomeCliente.concat(", obrigado por criar uma conta em nosso banco, sua agência é "+agencia).concat(", conta "+numeroConta).concat(" e seu saldo "+saldo+" já está disponível para saque."));
     }
 }
